45638e6699bac37a8e1f1c79ecd0d809d9b68907
shun.chida <chida-shun-hz@ynu.jp>
2022-09-24 03:37:45 +0000

    revision seeder and add pagination

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -13,7 +13,7 @@ class TasksTableSeeder extends Seeder
     {
         for($i = 1; $i <= 100; $i++) {
             DB::table('tasks')->insert([
-                'status' => 'test status ' . $i,
+                'status' => 'test ' . $i,
                 'content' => 'test content ' . $i
             ]);
         }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -25,6 +25,9 @@
         </table>
     @endif
     
+    {{-- ページネーションのリンク --}}
+    {{ $tasks->links() }}
+    
     {{-- メッセージ作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'btn btn-primary']) !!}
     
