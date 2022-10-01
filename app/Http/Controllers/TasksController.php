4d9dc58b8b08c39e757f8f2c664cf5106b1e93f3
shun.chida <chida-shun-hz@ynu.jp>
2022-10-01 06:09:01 +0000

    post

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -17,13 +17,20 @@ class TasksController extends Controller
     // getでtasks/にアクセスされた場合の「一覧表示処理」
     public function index()
     {
-        // タスク一覧を取得
-        $tasks = Task::paginate(25);
+        $data = [];
+        if (\Auth::check()) { // 認証済みの場合
+            // 認証済みユーザを取得
+            $user = \Auth::user();
+            // ユーザのタスクの一覧を作成日時の降順で取得
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(25);
+
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+        }
         
-        // タスク一覧ビューでそれを表示
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+        return view('welcome', $data);
     }
 
     /**
@@ -38,9 +45,13 @@ class TasksController extends Controller
         $task = new Task;
         
         // タスク作成ビューを表示
-        return view('tasks.create', [
-            'task' => $task,
-        ]);
+        if (\Auth::check()) {
+            return view('tasks.create', [
+                'task' => $task,
+            ]);
+        }
+        
+        return redirect('/');
     }
 
     /**
@@ -59,11 +70,11 @@ class TasksController extends Controller
             'content' => 'required|max:255',
         ]);
         
-        // タスクを作成
-        $task = new Task;
-        $task->status = $request->status;
-        $task->content = $request->content;
-        $task->save();
+        // 認証済みユーザ（閲覧者）のタスクを作成
+        $request->user()->tasks()->create([
+            'status' => $request->status,
+            'content' => $request->content,
+        ]);
 
         // トップページへリダイレクトさせる
         return redirect('/');
@@ -80,12 +91,16 @@ class TasksController extends Controller
     public function show($id)
     {
         // idの値でタスクを検索して取得
-        $task = Task::findOrFail($id);
+        $task = \App\Task::findOrFail($id);
 
         // タスク詳細ビューでそれを表示
-        return view('tasks.show', [
-            'task' => $task,
-        ]);
+        if (\Auth::id() === $task->user_id) {
+            return view('tasks.show', [
+                'task' => $task,
+            ]);
+        }
+        
+        return redirect('/');
     }
 
     /**
@@ -99,12 +114,16 @@ class TasksController extends Controller
     public function edit($id)
     {
         // idの値でタスクを検索して取得
-        $task = Task::findOrFail($id);
+        $task = \App\Task::findOrFail($id);
 
         // タスク編集ビューでそれを表示
-        return view('tasks.edit', [
-            'task' => $task,
-        ]);
+        if (\Auth::id() === $task->user_id) {
+            return view('tasks.edit', [
+                'task' => $task,
+            ]);
+        }
+        
+        return redirect('/');
     }
 
     /**
@@ -125,7 +144,7 @@ class TasksController extends Controller
         ]);
         
         // idの値でタスクを検索して取得
-        $task = Task::findOrFail($id);
+        $task = \App\Task::findOrFail($id);
         // タスクを更新
         $task->status = $request->status;
         $task->content = $request->content;
@@ -147,8 +166,10 @@ class TasksController extends Controller
     {
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // タスクを削除
+        // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
+        if (\Auth::id() === $task->user_id) {
         $task->delete();
+        }
 
         // トップページへリダイレクトさせる
         return redirect('/');
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,34 +1,29 @@
-@extends('layouts.app')
+<h1>タスク一覧</h1>
 
-@section('content')
+@if (count($tasks) > 0)
+    <table class="table table-striped">
+        <thread>
+            <tr>
+                <th>id</th>
+                <th>ステータス</th>
+                <th>タスク</th>
+            </tr>
+        </thread>
+        <tbody>
+            @foreach ($tasks as $task)
+            <tr>
+                <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                <td>{{ $task->status }}</td>
+                <td>{{ $task->content }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endif
 
-    <h1>タスク一覧</h1>
+{{-- ページネーションのリンク --}}
+{{ $tasks->links() }}
+
+{{-- タスク作成ページへのリンク --}}
+{!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'btn btn-primary']) !!}
     
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thread>
-                <tr>
-                        <th>id</th>
-                        <th>ステータス</th>
-                        <th>タスク</th>
-                </tr>
-            </thread>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-    
-    {{-- ページネーションのリンク --}}
-    {{ $tasks->links() }}
-    
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'btn btn-primary']) !!}
-    
-@endsection
